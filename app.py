cdc31540358157e44e1538b2da18423f0807f75f
Gabe571 <gabriel.castro1028@gmail.com>
2021-09-08 19:51:26 -0700

    inputs int + string

--- app.py
+++ app.py
@@ -2,3 +2,7 @@
 # age = input('What is your age? ') #
 # print('Hello '+ name) #
 # print(age) #
+
+# birth_year = input('Enter your birth year: ') #
+# age = 2021 - int(birth_year) #
+# print(age) #
