52a1cbcfac9b3aafae93d723e0998ec588c55ff8
Gabe571 <gabriel.castro1028@gmail.com>
2021-10-27 16:18:34 -0700

    inputs int + string

---
+++ Madlib.py
@@ -0,0 +1,17 @@
+# MADLIB
+
+# youtuber = 'Gabriel Castro'
+
+# print('subscribe to ' + youtuber)
+# print('subscribe to {}'.format(youtuber))
+# print(f'subscribe to {youtuber}')
+
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+famous_person = input('Famous person: ')
+
+madlib = f'Computer programming is so {adj}! I love to {verb1}.  Stay hydrated and {verb2} like you are \
+{famous_person}!'
+
+print(madlib)
