0183e16bf3f4f3eec1724b336418ae940a8fbddd
Gabe571 <gabriel.castro1028@gmail.com>
2021-10-27 18:19:54 -0700

    inputs int + string

--- Guess the Number.py
+++ Guess the Number.py
@@ -6,6 +6,10 @@ def guess(x):
     guess = 0
     while guess != random_number:
         guess = int(input(f'Guess a random number number between 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry, guess again, to low.')
+        elif guess > random_number:
+            print('Sorry, guess again, too high')
     print(f'Yay, you have guess the number {random_number} correctly')
 
 
